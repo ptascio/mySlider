21c9854074a2a98370612c3561bb5d764b0a2364
Peter Tascio <peter.tascio@gmail.com>
2017-09-20 12:32:42 -0400

    Add css file

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert("hi");
+var photos = [
+  "https://cdn.shopify.com/s/files/1/0657/6821/products/1_248d476a-bac9-4e8b-ab41-448c5242fc82_grande.jpg?v=1489761307"
+];
+
+var currentImg = document.getElementById("slider");
+var img = document.createElement("img");
+img.setAttribute("src", photos[0]);
+currentImg.appendChild(img);
